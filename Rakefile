7da54bc1b8b7a24c4c96f1c9d1eac5ecc70e7566
Rygel-XVI <micha232@umn.edu>
2018-03-02 19:34:37 -0800

    migrated databases, setup first view and getter

--- Rakefile
+++ Rakefile
@@ -2,3 +2,7 @@ task :default => :spec
 
 require_relative './config/environment'
 require 'sinatra/activerecord/rake'
+
+task :console do
+    Pry.start
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "myhouseiscold"
+  end
+
+  get '/' do
+    erb :index
+  end
 end
