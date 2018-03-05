0eb02d19de84e1df8a915a0dadc0ad9d37654df8
Rygel-XVI <micha232@umn.edu>
2018-03-04 17:53:38 -0800

    filled out genre routes

--- app/controllers/genre_controller.rb
+++ app/controllers/genre_controller.rb
@@ -9,27 +9,36 @@ class GenreController < ApplicationController
   end
 
   get '/genres/create' do
-    goes to create page
+    erb :'/genres/create_genre'
   end
 
   get '/genres/:slug/edit' do
-    goes to edit page
+    @genre = Genre.find_by_slug(params[:slug])
+    erb :'/genres/edit_genre'
   end
 
   get '/genres/:slug' do
-    goes to show page
+    @genre = Genre.find_by_slug(params[:slug])
+    erb :'/genres/show_genre'
   end
 
   post '/genres/create' do
-
+    if !params[:name].empty?
+      @genre = Genre.create(name: params[:name])
+    end
+    redirect to "/genres/#{@genre.slug}"
   end
 
   patch '/genres/:slug/edit' do
-
+    @genre = Genre.find_by_slug(params[:slug])
+    if !params[:name].empty?
+      @genre.update(name: params[:name])
+    end
+    redirect to "/genres/#{@genre.slug}"
   end
 
   delete '/genres/:slug/delete' do
-    
+
   end
 
 
