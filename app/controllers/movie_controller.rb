4ca789bc17bdbbe34e31a21ff5fefd8480c22ef1
Rygel-XVI <micha232@umn.edu>
2018-03-05 20:51:45 -0800

    created movie routes

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,3 +1,40 @@
 class MovieController < ApplicationController
 
+  get '/movies' do
+    erb :'/movies/show'
+  end
+
+  get '/movies/create_movie' do
+    erb :'/movies/create_movie'
+  end
+
+  get '/movies/:slug/edit_movie' do
+    @movie = movie.find_by_slug(params[:slug])
+    erb :'/movies/edit_movie'
+  end
+
+  get '/movies/:slug' do
+    @movie = movie.find_by_slug(params[:slug])
+    erb :'/movies/show_movie'
+  end
+
+  post '/movies/create_movie' do
+    if !params[:name].empty?
+      @movie = movie.create(name: params[:name])
+    end
+    redirect to "/movies/#{@movie.slug}"
+  end
+
+  patch '/movies/:slug/edit_movie' do
+    @movie = movie.find_by_slug(params[:slug])
+    if !params[:name].empty?
+      @movie.update(name: params[:name])
+    end
+    redirect to "/movies/#{@movie.slug}"
+  end
+
+  delete '/movies/:slug/delete' do
+
+  end
+
 end
